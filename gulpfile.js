ef747cc33ce70ef78882c6c7f5790bc7aeeb2d71
Igor Pokorny <igor.pokorny.1@gmail.com>
2017-08-19 21:39:19 +0200

    added "election-result.json" + mustache into html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    mustache = require("gulp-mustache")
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('default', ['html'])
+
+gulp.task('watch', function() {
+  gulp.watch('src/*', ['html']);
+})
+
+gulp.task('html', function() {
+  return gulp.src('./src/sr-volby.html')
+    .pipe(mustache('./src/election-results.json', {}, {}))
+    .pipe(gulp.dest('.'))
+})
