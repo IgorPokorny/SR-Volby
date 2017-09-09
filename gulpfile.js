366139d39bd289bf33f6820a1bb3406f67cadb8e
Igor Pokorny <igor.pokorny.1@gmail.com>
2017-09-09 19:23:30 +0200

    Add JS code counting chart_width + formating numbers

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+
+
 var gulp = require('gulp'),
     mustache = require("gulp-mustache"),
     fs = require('fs')
@@ -21,5 +23,25 @@ gulp.task('template', function() {
  *   - format numbers
  */
 function prepareResults(results) {
-    return results  // TODO: Implement processing
+    for(var i = 0; i < results.parliament.length; i++) {      
+        var parties = results.parliament[i].parties
+        for (var j = 0; j < parties.length; j++) {
+            var percents = parties[j].votes_p
+            var chart = percents * 4
+            parties[j].votes_p = formatNumber(percents)
+            parties[j].chart_width = chart
+            // parties[j].elected = percents >= 5
+            if (percents < 5) {
+                parties[j].elected = false
+            } else {
+                parties[j].elected = true
+            }
+        }
+    } 
+    return results
 }
+
+function formatNumber(number) {
+    return number.toLocaleString('sk', { useGrouping: true, minimumFractionDigits: 2 })
+}
+
