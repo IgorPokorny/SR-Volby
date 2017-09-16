e238d2d01e23aa2f784e353385e4178eaea7c599
Sergej Chodarev <sergej.chodarev@yandex.com>
2017-09-16 16:34:03 +0200

    Formatter: Replace HTML entities with Unicode characters

--- formatter.js
+++ formatter.js
@@ -1,10 +1,12 @@
 module.exports.formatNumber = formatNumber
 
+const FIGURE_SPACE = '\u2007'
+const THIN_SPACE = '\u2009'
+
 function formatNumber(number, integerLength=0, fractionLength=0) {
     var result = number.toFixed(fractionLength).replace('.', ',')
     result = padWithSpaces(result, integerLength, fractionLength)
     result = insertGroupSeparators(result, fractionLength)
-    result = result.replace(/ /g, '&#8199;') // Figure spaces
     return result
 }
 
@@ -14,7 +16,7 @@ function padWithSpaces(numberStr, integerLength=0, fractionLength=0) {
         paddingLength += 1 + fractionLength
     }
     if (paddingLength > 0) {
-        return ' '.repeat(paddingLength) + numberStr
+        return FIGURE_SPACE.repeat(paddingLength) + numberStr
     } else {
         return numberStr
     }
@@ -28,7 +30,7 @@ function insertGroupSeparators(numberStr, fractionLength) {
     var result = numberStr.slice(-firstGroupLength)
     var rest = numberStr.slice(0, -firstGroupLength)
     while (rest.length > 0) {
-        result = rest.slice(-3) + '&thinsp;' + result
+        result = rest.slice(-3) + THIN_SPACE + result
         rest = rest.slice(0, -3)
     }
     return result
@@ -41,13 +43,13 @@ function testFormatNumber() {
     assert.strictEqual(formatNumber(1.5), '2')
     assert.strictEqual(formatNumber(1, 0, 2), '1,00')
     assert.strictEqual(formatNumber(1.125, 0, 2), '1,13')
-    assert.strictEqual(formatNumber(1000), '1&thinsp;000')
-    assert.strictEqual(formatNumber(100000), '100&thinsp;000')
-    assert.strictEqual(formatNumber(1000000), '1&thinsp;000&thinsp;000')
-    assert.strictEqual(formatNumber(1, 3), '&#8199;&#8199;1')
-    assert.strictEqual(formatNumber(1, 4), '&#8199;&thinsp;&#8199;&#8199;1')
+    assert.strictEqual(formatNumber(1000), '1\u2009000')
+    assert.strictEqual(formatNumber(100000), '100\u2009000')
+    assert.strictEqual(formatNumber(1000000), '1\u2009000\u2009000')
+    assert.strictEqual(formatNumber(1, 3), '\u2007\u20071')
+    assert.strictEqual(formatNumber(1, 4), '\u2007\u2009\u2007\u20071')
     assert.strictEqual(formatNumber(10000.5, 7, 2),
-                       '&#8199;&thinsp;&#8199;10&thinsp;000,50')
+                       '\u2007\u2009\u200710\u2009000,50')
 }
 
 if (require.main === module) {
